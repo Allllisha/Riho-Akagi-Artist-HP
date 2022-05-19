693e4acd6a617cae286a79b3748cf09de743e987
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-05-19 23:45:03 +0900

    changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import Arrow from "../public/arrow.png"
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -11,7 +11,29 @@ export default function Home() {
         <meta name="description" content="Riho Akagi Pianist" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Riho Akagi</p>
+      <div className={styles.hero}>
+        <div className={styles.slider}>
+          <div className={styles.image}></div>
+          <div className={styles.image}></div>
+          <div className={styles.image}></div>
+          <div className={styles.boxString}>
+            <div className={styles.indexName}>
+               <h3>Pianist</h3>
+              <div className={styles.indexAllName}>
+              <div className={styles.firstname}>
+            <h1>Riho</h1>
+            </div>
+            <div className={styles.lastname}>
+            <h1>Akagi</h1>
+            </div>
+            </div>
+            <div className={styles.blink}>
+           <Image src={Arrow} alt="" />
+           </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
